8194409ec790a52c044a1543d301a8e7961ffe24
sammdu <hmu@sammdu.com>
2021-10-31 17:09:09 -0400

    fixed a few bugs with GSON and HTTP requests; refactored queryApi() method.

--- src/main/java/tech/autodirect/api/ApiEndpoints.java
+++ src/main/java/tech/autodirect/api/ApiEndpoints.java
@@ -44,25 +44,29 @@ public class ApiEndpoints {
 
 	// demo API endpoint; use for reference
 	@GetMapping("/demo")
-	public Object hello(@RequestParam String loanAmount, @RequestParam String creditScore, @RequestParam String budget,
-						@RequestParam String vehicleYear, @RequestParam String vehicleKms) {
-		// TODO: Remove this hardcoding and add params once frontend can send vehicle info.
+	public Object hello(@RequestParam String loanAmount, @RequestParam String creditScore, @RequestParam String budget) {
+
+		// TODO: Remove this hardcoding and read params from other parts of the program.
 		String vehicleMake = "Honda";
 		String vehicleModel = "Civic";
+		String vehicleYear = "2018";
+		String vehicleKms = "1";
+
 		try {
 			return SensoApi.queryApi(
-					loanAmount,
-					creditScore,
-					budget,
-					vehicleMake,
-					vehicleModel,
-					vehicleYear,
-					vehicleKms
+				loanAmount,
+				creditScore,
+				budget,
+				vehicleMake,
+				vehicleModel,
+				vehicleYear,
+				vehicleKms
 			);
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 			return "Server Error!";
 		}
+
 	}
 
 }
