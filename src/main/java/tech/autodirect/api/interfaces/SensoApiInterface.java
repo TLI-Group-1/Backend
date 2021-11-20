3d7060e254bb320d1cab41dbcdb0c36cfcfa7ad8
Shalev Lifshitz <lifshitz.shalev@gmail.com>
2021-11-19 22:29:09 -0500

    Updated SensoApiInterface with params and documentation

--- src/main/java/tech/autodirect/api/interfaces/SensoApiInterface.java
+++ src/main/java/tech/autodirect/api/interfaces/SensoApiInterface.java
@@ -4,5 +4,19 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface SensoApiInterface {
-    public Map<String, Object> getLoanOffer() throws IOException, InterruptedException;
+    /**
+     * Query the Senso /rate Api to get a loan offer.
+     * @return A Map that includes the body of the query result as well as the status number of the https query.
+     */
+    public Map<String, Object> getLoanOffer(
+        String loanAmount,
+        String creditScore,
+        String budget,
+        String vehicleMake,
+        String vehicleModel,
+        String vehicleYear,
+        String vehicleKms,
+        String listPrice,
+        String downpayment
+    ) throws IOException, InterruptedException;
 }
