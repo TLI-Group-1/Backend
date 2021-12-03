2f4413bb91651b0823753fd079301f59aee6d25b
Shalev Lifshitz <lifshitz.shalev@gmail.com>
2021-12-03 17:29:38 -0500

    Rehauled SvcUserLoginTest and updated SvcUserLogin

--- src/main/java/tech/autodirect/api/services/SvcUserLogin.java
+++ src/main/java/tech/autodirect/api/services/SvcUserLogin.java
@@ -39,16 +39,15 @@ public class SvcUserLogin {
     public Map<String, Object> loginUser(String userId) throws SQLException {
         if (tableUsers.userExists(userId)) {
             // userId exists, return existing user info
-            Map<String, Object> userInfo = tableUsers.getUserByID(userId);
-            userInfo.remove("offers_table");
-            return userInfo;
+            Map<String, Object> userMap = tableUsers.getUserByID(userId);
+            userMap.remove("offers_table");
+            return userMap;
         } else {
             // userId does not exist, create new user with default info
             int creditScore = bankApi.getCreditScore(userId);
-            String offersTableName = TableOffersInterface.createTableName(userId);
             double defaultDownPayment = 1000; // TODO: update?
             double defaultBudgetMonthly = 250; // TODO: update?
-            tableUsers.addUser(userId, creditScore, defaultDownPayment, defaultBudgetMonthly, offersTableName);
+            tableUsers.addUser(userId, creditScore, defaultDownPayment, defaultBudgetMonthly);
 
             // Return user info from database
             Map<String, Object> userMap = tableUsers.getUserByID(userId);
--- src/test/java/tech/autodirect/api/services/SvcUserLoginTest.java
+++ src/test/java/tech/autodirect/api/services/SvcUserLoginTest.java
@@ -1,22 +1,63 @@
 package tech.autodirect.api.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import tech.autodirect.api.database.TableUsers;
+import tech.autodirect.api.interfaces.BankApiInterface;
 import tech.autodirect.api.interfaces.TableUsersInterface;
 import tech.autodirect.api.upstream.BankApi;
 
 import java.sql.SQLException;
 import java.util.Map;
 
+
+// This annotation allows us to use a non-static BeforeAll/AfterAll methods (TODO: check if ok)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SvcUserLoginTest {
-    @Test void testLoginUser(){
-        try{
-            SvcUserLogin loginClass = new SvcUserLogin(new TableUsers("autodirect"), new BankApi());
-            String username = new String("username_member824");
-        }
-        catch(SQLException e) {
+    private static final String dbName = "autodirect";
+    private final String testUserId = "SvcUserLoginTest.test_user.700";
+    private TableUsersInterface tableUser;
+    private SvcUserLogin svcUserLogin;
+
+    @Test
+    void testSearchCarsAllNull() {
+        try {
+            Map<String, Object> userMap = svcUserLogin.loginUser(testUserId);
+            assert userMap.size() > 0;
+            assert userMap.get("user_id").equals(testUserId);
+            assert tableUser.userExists(testUserId);
+        } catch (SQLException e) {
             e.printStackTrace();
             assert false;
+        }
+    }
+
+    @BeforeAll
+    public void setUpAll() {
+        try {
+            this.tableUser = new TableUsers(dbName);
+            BankApiInterface bankApi = new BankApi();
+            this.svcUserLogin = new SvcUserLogin(tableUser, bankApi);
+
+            // testUserId user will be created in tests, ensure it doesn't exist yet
+            if (tableUser.userExists(testUserId)) {
+                tableUser.removeUserByID(testUserId);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void tearDownEach() {
+        try {
+            if (tableUser.userExists(testUserId)) {
+                tableUser.removeUserByID(testUserId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
