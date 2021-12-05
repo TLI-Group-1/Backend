e08a7385497b3e46d50797f091478c7b145f83f2
Shalev Lifshitz <lifshitz.shalev@gmail.com>
2021-12-05 17:15:12 -0500

    took out IF NOT EXIST from newTable table creation (want it to throw if already exists)

--- src/main/java/tech/autodirect/api/database/TableOffers.java
+++ src/main/java/tech/autodirect/api/database/TableOffers.java
@@ -61,7 +61,7 @@ public class TableOffers extends Table implements TableOffersInterface {
         // create and execute the SQL statement that will create an offer table
         Statement stmt = this.dbConn.createStatement();
         stmt.executeUpdate(
-            "CREATE TABLE IF NOT EXISTS " + this.schemaName + "." + this.tableName + " (" +
+            "CREATE TABLE " + this.schemaName + "." + this.tableName + " (" +
                 "offer_id       serial      NOT NULL PRIMARY KEY, " +
                 "car_id         integer     NOT NULL, " +
                 "loan_amount    decimal(12) NOT NULL, " +
