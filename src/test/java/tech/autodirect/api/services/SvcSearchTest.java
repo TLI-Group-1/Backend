7e7e7c281b6510f948a7107a0bfa2149b78e0be2
Shalev Lifshitz <lifshitz.shalev@gmail.com>
2021-12-09 02:13:12 -0500

    Made epic tests

--- src/test/java/tech/autodirect/api/services/SvcSearchTest.java
+++ src/test/java/tech/autodirect/api/services/SvcSearchTest.java
@@ -10,11 +10,14 @@ import tech.autodirect.api.interfaces.TableCarsInterface;
 import tech.autodirect.api.interfaces.TableOffersInterface;
 import tech.autodirect.api.interfaces.TableUsersInterface;
 import tech.autodirect.api.upstream.SensoApi;
+import tech.autodirect.api.utils.TypeConvert;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 // This annotation allows us to use a non-static BeforeAll/AfterAll methods (TODO: check if ok)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -25,37 +28,52 @@ class SvcSearchTest {
     private SvcSearch svcSearch;
 
     @Test
-    void testSearchCarsInvalidAllNull() {
+    void testSearchCarsPreLoginWithAllNull() {
         try {
-            List<Map<String, Object>> carsResult = svcSearch.searchCars("", "null", "null", "null", "null");
+            // Since userId is "", only checks sortBy
+            List<Map<String, Object>> carsResult = svcSearch.search("", "null", "null", "null", "null");
             assert carsResult.size() > 0;
         } catch (IOException | InterruptedException | SQLException e) {
             e.printStackTrace();
             assert false;
         } catch (ResponseStatusException e) {
-            assert true; // check that 400 error is thrown
+            assert Objects.requireNonNull(e.getMessage()).startsWith("400 BAD_REQUEST");
         }
     }
 
     @Test
-    void testSearchCarsInvalidAllEmpty() {
+    void testSearchCarsPreLoginWithAllEmpty() {
         try {
-            List<Map<String, Object>> carsResult = svcSearch.searchCars("", "", "", "", "");
+            List<Map<String, Object>> carsResult = svcSearch.search("", "", "", "", "");
             assert carsResult.size() > 0;
         } catch (IOException | InterruptedException | SQLException e) {
             e.printStackTrace();
             assert false;
         } catch (ResponseStatusException e) {
-            assert true; // check that 400 error is thrown
+            assert Objects.requireNonNull(e.getMessage()).startsWith("400 BAD_REQUEST");
         }
     }
 
     @Test
-    void testSearchCarsPreLogin() {
+    void testSearchCarsPreLoginNonPriceSortBy() {
         try {
             tableUsers.addUser(testUserId, 700, 1000, 200);
-            List<Map<String, Object>> carsResult = svcSearch.searchCars("", "1000", "200", "price", "true");
+            List<Map<String, Object>> carsResult = svcSearch.search("", "1000", "200", "term_length", "true");
             assert carsResult.size() > 0;
+        } catch (IOException | InterruptedException | SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            assert false;
+        } catch (ResponseStatusException e) {
+            // pre-login search must take "price" sortBy
+            assert Objects.requireNonNull(e.getMessage()).startsWith("400 BAD_REQUEST");
+        }    }
+
+    @Test
+    void testSearchCarsPreLoginCheckSorting() {
+        try {
+            List<String> valuesOfSortBy = List.of("price");
+            List<String> valuesOfSortAsc = Arrays.asList("true", "false");
+            testSorting("", valuesOfSortBy, valuesOfSortAsc);
         } catch (IOException | InterruptedException | SQLException | ClassNotFoundException e) {
             e.printStackTrace();
             assert false;
@@ -63,26 +81,48 @@ class SvcSearchTest {
     }
 
     @Test
-    void testSearchCarsPostLogin() {
+    void testSearchCarsPostLoginCheckSorting() {
         try {
-            tableUsers.addUser(testUserId, 700, 1000, 200);
-            List<Map<String, Object>> carsResult = svcSearch.searchCars(testUserId, "1000", "200", "price", "true");
-            assert carsResult.size() > 0;
+            List<String> valuesOfSortBy = Arrays.asList("price", "payment_mo", "apr", "total_sum", "term_length");
+            List<String> valuesOfSortAsc = Arrays.asList("true", "false");
+            testSorting(testUserId, valuesOfSortBy, valuesOfSortAsc);
         } catch (IOException | InterruptedException | SQLException | ClassNotFoundException e) {
             e.printStackTrace();
             assert false;
         }
     }
 
-    @Test
-    void testSearchCarsPostLoginBadSortBy() {
-        try {
-            tableUsers.addUser(testUserId, 700, 1000, 200);
-            List<Map<String, Object>> carsResult = svcSearch.searchCars(testUserId, "1000", "200", "price", "true");
-            assert carsResult.size() > 0;
-        } catch (IOException | InterruptedException | SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            assert false;
+    /**
+     * Tests that sorting works looping over valuesOfSortBy and valuesOfSortAsc with userId.
+     */
+    void testSorting(
+            String userId,
+            List<String> valuesOfSortBy,
+            List<String> valuesOfSortAsc
+    ) throws SQLException, ClassNotFoundException, IOException, InterruptedException {
+        for (String sortBy : valuesOfSortBy) {
+            for (String sortAscString : valuesOfSortAsc) {
+                if (!tableUsers.checkUserExists(userId) && !userId.equals("")) {
+                    tableUsers.addUser(userId, 700, 1000, 200);
+                }
+
+                List<Map<String, Object>> carsAndOffers = svcSearch.search(
+                        userId, "1000", "200", sortBy, sortAscString
+                );
+                assert carsAndOffers.size() > 0;
+
+                boolean sortAsc = Boolean.parseBoolean(sortAscString);
+                double prev = TypeConvert.toDouble(carsAndOffers.get(0).get(sortBy));
+                for (Map<String, Object> carAndOffer : carsAndOffers) {
+                    double valueDouble = TypeConvert.toDouble(carAndOffer.get(sortBy));
+
+                    if (sortAsc) {
+                        assert prev <= valueDouble;
+                    } else {
+                        assert prev >= valueDouble;
+                    }
+                }
+            }
         }
     }
 
