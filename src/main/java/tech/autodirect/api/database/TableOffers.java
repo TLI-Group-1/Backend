c966fd0ce9ff6dd30dfcf0b3c1d9ec9a35c33c8a
Nada G. Eldin <nada.geldin@gmail.com>
2021-12-12 02:58:32 -0500

    Finished Feasible tests for tables and fixrd some test names

--- src/main/java/tech/autodirect/api/database/TableOffers.java
+++ src/main/java/tech/autodirect/api/database/TableOffers.java
@@ -199,7 +199,6 @@ public class TableOffers extends Table implements TableOffersInterface {
             );
             stmt.executeUpdate();
             stmt.close();
-        } else {
         }
     }
 
